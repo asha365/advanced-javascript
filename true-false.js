a01c4dfdb7dc7adfb87ec88d6b0de9002167bad8
Fatima (Asha Afrin) <asha.engrbbd@gmail.com>
2020-11-08 05:21:38 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,34 @@
+//condition check step by step
+// const age = 4;
+// if(age > 0){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+//condition check step by step
+//true-false string check
+// const name = 'asha';
+// if(name){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+//condition check step by step
+//const name = 'asha';
+//let name = null;
+//let name = NaN;
+//let name = [];
+//console.log(name);]
+//let name = {};
+
+let name = 0;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
