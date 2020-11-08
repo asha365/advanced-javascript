d02f7c825d32159126264c957416986594753a53
Fatima (Asha Afrin) <asha.engrbbd@gmail.com>
2020-11-08 13:00:09 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,27 @@
+//undefined
+//let name;
+//console.log(name);
+
+//undefined
+// function add(num1,num2){
+//     console.log(num1+num2);
+// }
+
+// function add(num1,num2){
+//     console.log(num2);
+// }
+//const result = add(10,20);
+// const result = add(10);
+// console.log(result);
+
+//undefined
+const parson = {name: "anwoar hossain", phone:0837398};
+console.log(parson.gf);
+
+//undefined
+let fun = undefined;
+console.log(fun);
+
+//undefined
+let ages = [2,4,5];
+console.log(ages[10]);
